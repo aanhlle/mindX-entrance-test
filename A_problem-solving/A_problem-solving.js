e67a506dfbe75d4aef9355e0631bd1fadcc6af6f
aanhlle <levietanh1112@gmail.com>
2022-01-15 16:23:07 +0700

    typo fixed

--- A_problem-solving/A_problem-solving.js
+++ A_problem-solving/A_problem-solving.js
@@ -31,8 +31,8 @@ allLongestStrings(["BINH", "HUNG", "PHUOC", "CAO", "KHANH"]);
 allLongestStrings(["BINH", "HUNG", "PHUOC", "CAO", "KHANH", "KHANH"]);
 
 /*
- Có một nhóm người đang đứng thành một hàng, để chia ra làm 2 đội từ hàng người thì quản trò chia như sao. Người đứng thứ nhất vào Team 1, người đúng thứ hai vào Team 2, người đứng thứ ba vào lại Team 1, cứ tiếp tục như thế cho đến người cuối cùng.
- Viết chương trình có đầu vào là một mảng chưa cân nặng của tất cả mọi người theo thứ tự hàng ban đầu và yêu cầu trả về mảng chưa tổng cân nặng của 2 team.
+ Có một nhóm người đang đứng thành một hàng, để chia ra làm 2 đội từ hàng người thì quản trò chia như sau. Người đứng thứ nhất vào Team 1, người đúng thứ hai vào Team 2, người đứng thứ ba vào lại Team 1, cứ tiếp tục như thế cho đến người cuối cùng.
+ Viết chương trình có đầu vào là một mảng chứa cân nặng của tất cả mọi người theo thứ tự hàng ban đầu và yêu cầu trả về mảng chưa tổng cân nặng của 2 team.
  
  Ví dụ: INPUT [60, 40, 55, 75, 64] thì OUTPUT là [179, 115]
  
